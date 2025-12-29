d4aae35bb88dcac5684db6eb47bfb73873551deb
tromoSM <tromosm7@gmail.com>
2025-12-30 01:41:53 +0530

    main.py
    Refactor paths for encrypted files and images, and add error handling for user permissions.

--- main.py
+++ main.py
@@ -9,7 +9,35 @@ import webview as webfucker3000
 import os as abspath
 from flask import render_template,Flask,send_file,request
 from cryptography.fernet import Fernet,InvalidToken
+import ctypes as diddyb1ud
+import threading as dih
 
+def Error(x):
+    diddyb1ud.windll.user32.MessageBoxW(0,x,"CryptivisX",0x10)
+def Info(dih):
+    diddyb1ud.windll.user32.MessageBoxW(0,dih,"CryptivisX",0x40)
+def Warning(dih):
+    diddyb1ud.windll.user32.MessageBoxW(0,dih,"CryptivisX",0x30)
+def Ask(dih,nih,**l):
+    if nih:
+        a=diddyb1ud.windll.user32.MessageBoxW(0,dih,"CryptivisX",0x20|0x4)
+    else:
+        a=diddyb1ud.windll.user32.MessageBoxW(0,dih,"CryptivisX",0x20)
+    if a==6 and "Yes" in l:
+            l["Yes"]()
+    elif a==7 and "No" in l:
+            l["No"]()
+def tstahhh():
+            try:
+               return diddyb1ud.windll.shell32.IsUserAnAdmin()
+            except:
+                return False
+if not tstahhh():
+         diddyb1ud.windll.shell32.ShellExecuteW(
+            None,'runas',johnpork.executable," ".join(johnpork.argv),
+            None,1
+         )
+         johnpork.exit(0)
 def rpath(r):
     if hasattr(johnpork,"_MEIPASS"):
         return abspath.path.join(johnpork._MEIPASS,r)
@@ -17,6 +45,17 @@ def rpath(r):
 base=abspath.getenv("APPDATA")or abspath.path.expanduser('~')
 APPDATA=wishIKnew(base)/"CryptivisX"
 APPDATA.mkdir(exist_ok=True)
+IMIM_PIC_DATA=wishIKnew(abspath.path.expanduser('~'))/'Pictures'/'CryptivisX'
+IMIM_PIC_DATA.mkdir(parents=True,exist_ok=True)
+def after():
+ def afterdih():
+    try:
+        abspath.startfile(str(IMIM_PIC_DATA))
+    except Exception as dihdih:
+        print('.NET Error : ',dihdih)
+        Warning(dihdih)
+ dih.Thread(target=afterdih,daemon=True).start()
+    
 encodeSHIT=Flask(__name__)
 @encodeSHIT.route('/')
 def dihtouch():
@@ -67,17 +106,18 @@ class nih():
         with open(self.imim_path,"rb")as dihfile:
             imim_data=dihfile.read()
         llkv=uia.encrypt(imim_data)
-        imim_last=APPDATA/('[encrypted by CryptivisX]'+abspath.path.basename(self.imim_path))
+        imim_last=IMIM_PIC_DATA/('[encrypted by CryptivisX]'+abspath.path.basename(self.imim_path))
         with open(imim_last,"wb")as dihfile:
          dihfile.write(llkv)
         try:
             abspath.remove(self.imim_path)
         except PermissionError:
             letme.sleep(2)
-            
+        
         kyq=diddyblud.make_qr(yoyO)
         kyq.save(abspath.path.join(APPDATA,"tempQR.svg"),scale=6.7)
         dihsuc.copy(APPDATA/"tempQR.svg",rpath("static/tempQR.svg"))
+        after()
         return yoyO
 
     def retrJS(self,tx):
@@ -100,16 +140,19 @@ class nih():
         except InvalidToken:
          print('INVALID KEY ERROR')
          return "Invalid key. Please try a different key."    
-        imim_last=APPDATA/abspath.path.basename(self.imim_repath).replace("[encrypted by CryptivisX]","")
+        imim_last=IMIM_PIC_DATA/abspath.path.basename(self.imim_repath).replace("[encrypted by CryptivisX]","")
         with open(imim_last,"wb")as dihfile:
          dihfile.write(imim_inside)
         try:
             abspath.remove(self.imim_repath)
             print('SUCCESS - DE')
+            after()
         except PermissionError:
             letme.sleep(2)
+            after()
             return "Permission Error attempting to retry"
         except:
+            Warning('Permission Error - retrying attempt failed.')
             print("ERROR PERSMISSION ERROR")
             return "Permission Error retrying attempt failed."
 @encodeSHIT.route('/imim-preview')
